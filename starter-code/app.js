a8d530f6fcf7132df6e72cb31d322a91e1cb588d
ZebGirouard <timzebgir@gmail.com>
2016-11-20 20:35:27 -0700

    Adding starter code

---
+++ starter-code/app.js
@@ -0,0 +1,17 @@
+//Did you use npm install to
+//add all these packages
+//to our project?
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// How do we 'require' the candyRouter file?
+var candyRouter;
+
+app.use(bodyParser.json());
+
+//How do we redirect the /candies path
+//through our candyRouter?
+//Hint: you need app.use
+
+app.listen(3000);
---
+++ starter-code/candyRouter.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+router = express.Router();
+
+var candies;
+
+//What would need to go into candies
+//in order to pass our first test?
+
+router.get('/', function(req,res) {
+	// What would go here? 
+	// Hint: we want all candies in JSON format
+});
+
+// Fill out the rest of the routes here
+
+module.exports = router;
